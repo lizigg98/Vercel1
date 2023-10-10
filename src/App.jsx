e9d5ee7a264d39f690e35ea4cafd673cf364e024
lizigg98 <liputh2003@gmail.com>
2023-10-10 20:42:20 +0700

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import {Controller, useForm} from 'react-hook-form'
+import * as yup from "yup"
+import {yupResolver} from "@hookform/resolvers/yup"
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+     const schema = yup.object().shape({
+      receiver: yup.string().required("receiver is required"),
+      subject: yup.string().required("subject is required"),
+      content: yup.string().required("content is required"),
+      file: yup
+      .mixed()
+      .test("validate-file","File format is invalid", function (value) {
+        return ["image/jpeg", "image/png"].includes(value.type);
+      })
+
+     })
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      receiver: "",
+      subject: "",
+      content: "",
+      file: "",
+    },
+    resolver: yupResolver(schema),
+  });
+
+
+  const onSubmitFormCallback = (values) => {
+    console.log("values", values);
+  };
+
+  console.log("errors", errors);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+       <div>
+        <h1>Form</h1>
+        <form onSubmit={handleSubmit(onSubmitFormCallback)}>
+          <Controller 
+          name="receiver"
+          control={control}
+          render={({field}) => (
+            <div>
+               <input {...field} type='text' placeholder='Nguoi nhan' />
+            </div>
+          )} 
+          />
+           <Controller 
+          name="subject"
+          control={control}
+          render={({field}) => (
+            <div>
+               <input {...field} type='text' placeholder='Chu de' />
+            </div>
+          )} 
+          />
+           <Controller 
+          name="content"
+          control={control}
+          render={({field}) => (
+            <div>
+               <input {...field} type='text' placeholder='Noi dung' />
+            </div>
+          )} 
+          />
+          <Controller 
+          name="file"
+          control={control}
+          render={({field}) => (
+            <div>
+               <input {...field} type='file' value={field.value?.fileName} placeholder='Upload File'
+               onChange={(event) => {
+                field.onChange(event.target.files[0]);
+               }} />
+               <div>{errors?.file?.message}</div>
+            </div>
+          )} 
+          />
+          <button type='submit'>Submit</button>
+          </form>
+       </div>
   )
 }
+
 
 export default App
